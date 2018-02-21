f78981dd6bb77baa4b84bca61f636457e0b06e7d
kline379 <kline.379@osu.edu>
2018-02-21 12:55:18 -0500

    Initial commit.

---
+++ helpers.py
@@ -0,0 +1,11 @@
+'''
+pulls data from the data file and returns it.
+'''
+def get_data():
+    raise Exception('get_data not implemented.')
+
+'''
+Gets the initial paramaters
+'''
+def get_initial_params():
+    raise Exception('get_initial_params not implemented.')
